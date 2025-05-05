c15f0f03e87a0c81b96e9be2412e4909759a8315
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2025-05-05 19:15:57 +0900

    Add a MeterField

---
+++ Source/Codeer.LowCode.Bindings.ORiN3/Designs/MeterFieldDesign.cs
@@ -0,0 +1,24 @@
+﻿using Codeer.LowCode.Bindings.ORiN3.Components;
+using Codeer.LowCode.Bindings.ORiN3.Fields;
+using Codeer.LowCode.Blazor.OperatingModel;
+using Codeer.LowCode.Blazor.Repository.Data;
+using Codeer.LowCode.Blazor.Repository.Design;
+
+namespace Codeer.LowCode.Bindings.ORiN3.Designs
+{
+    [ToolboxIcon(PackIconMaterialKind = "ProgressStar")]
+    public class MeterFieldDesign() : FieldDesignBase(typeof(MeterFieldDesign).FullName!)
+    {
+        public int PollingTime { get; set; } = 1000;
+
+        //TODO percentage
+        [Designer]
+        public string MeterColor { get; set; } = "#ff4d4d";
+
+        public override string GetWebComponentTypeFullName() => typeof(MeterFieldComponent).FullName!;
+        public override string GetSearchWebComponentTypeFullName() => string.Empty;
+        public override string GetSearchControlTypeFullName() => string.Empty;
+        public override FieldBase CreateField() => new MeterField(this);
+        public override FieldDataBase? CreateData() => null;
+    }
+}
---
+++ Source/Codeer.LowCode.Bindings.ORiN3/Fields/MeterField.cs
@@ -0,0 +1,19 @@
+﻿using Codeer.LowCode.Bindings.ORiN3.Designs;
+using Codeer.LowCode.Blazor.DataIO;
+using Codeer.LowCode.Blazor.OperatingModel;
+using Codeer.LowCode.Blazor.Repository.Data;
+
+namespace Codeer.LowCode.Bindings.ORiN3.Fields
+{
+    public class MeterField(MeterFieldDesign design)
+        : FieldBase<MeterFieldDesign>(design)
+    {
+        public double Value { get; set; } = 0.75;
+
+        public override bool IsModified => false;
+        public override FieldDataBase? GetData() => null;
+        public override FieldSubmitData GetSubmitData() => new();
+        public override async Task InitializeDataAsync(FieldDataBase? fieldDataBase) => await Task.CompletedTask;
+        public override async Task SetDataAsync(FieldDataBase? fieldDataBase) => await Task.CompletedTask;
+    }
+}
