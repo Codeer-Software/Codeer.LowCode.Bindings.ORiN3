81fc601bf93ca7e1cae8c809077372019cd87ae0
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2025-05-05 23:40:11 +0900

    Adjust Color

--- Source/Codeer.LowCode.Bindings.ORiN3/Designs/MeterFieldDesign.cs
+++ Source/Codeer.LowCode.Bindings.ORiN3/Designs/MeterFieldDesign.cs
@@ -9,10 +9,6 @@ namespace Codeer.LowCode.Bindings.ORiN3.Designs
     [ToolboxIcon(PackIconMaterialKind = "ProgressStar")]
     public class MeterFieldDesign() : FieldDesignBase(typeof(MeterFieldDesign).FullName!)
     {
-        //TODO percentage
-        [Designer]
-        public string MeterColor { get; set; } = "#ff4d4d";
-
         public override string GetWebComponentTypeFullName() => typeof(MeterFieldComponent).FullName!;
         public override string GetSearchWebComponentTypeFullName() => string.Empty;
         public override string GetSearchControlTypeFullName() => string.Empty;
