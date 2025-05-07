1e0c24a315f2bcab4bf221805f35e80755837f69
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2025-05-07 23:08:33 +0900

    Adjustment

--- Source/Codeer.LowCode.Bindings.ORiN3/Designs/DeviceStateGanttChartFieldDesign.cs
+++ Source/Codeer.LowCode.Bindings.ORiN3/Designs/DeviceStateGanttChartFieldDesign.cs
@@ -9,6 +9,33 @@ namespace Codeer.LowCode.Bindings.ORiN3.Designs
     [ToolboxIcon(PackIconMaterialKind = "ProgressStar")]
     public class DeviceStateGanttChartFieldDesign() : FieldDesignBase(typeof(DeviceStateGanttChartFieldDesign).FullName!)
     {
+        [Designer]
+        public int StartHour { get; set; } = 9;
+        [Designer]
+        public int EndHour { get; set; } = 18;
+        [Designer]
+        public int StepMin { get; set; } = 10;
+
+        [Designer(CandidateType = CandidateType.Module)]
+        public string ModuleName { get; set; } = string.Empty;
+
+        [Designer(CandidateType = CandidateType.Field), ModuleMember(Member = $"{nameof(ModuleName)}")]
+        public string TimeField { get; set; } = string.Empty;
+
+        [Designer(CandidateType = CandidateType.Field), ModuleMember(Member = $"{nameof(ModuleName)}")]
+        public string DeviceField { get; set; } = string.Empty;
+
+        [Designer(CandidateType = CandidateType.Field), ModuleMember(Member = $"{nameof(ModuleName)}")]
+        public string StateField { get; set; } = string.Empty;
+
+        // TODO : Change the typef
+        [Designer(CandidateType = CandidateType.StringList)]
+        public List<string> DeviceAndMachinery { get; set; } = [];
+
+        // TODO : Change the typef
+        [Designer(CandidateType = CandidateType.StringList)]
+        public List<string> StateAndTextAndColor { get; set; } = [];
+
         public override string GetWebComponentTypeFullName() => typeof(DeviceStateGanttChartFieldComponent).FullName!;
         public override string GetSearchWebComponentTypeFullName() => string.Empty;
         public override string GetSearchControlTypeFullName() => string.Empty;
--- Source/Codeer.LowCode.Bindings.ORiN3/Fields/DeviceStateGanttChartField.cs
+++ Source/Codeer.LowCode.Bindings.ORiN3/Fields/DeviceStateGanttChartField.cs
@@ -6,6 +6,10 @@ using Codeer.LowCode.Blazor.Script;
 
 namespace Codeer.LowCode.Bindings.ORiN3.Fields
 {
+    /* ===== Data model ===== */
+    public record Segment(string Color);
+    public record MachineRow(string Name, List<Segment> Segments);
+
     public class DeviceStateGanttChartField(DeviceStateGanttChartFieldDesign design)
         : FieldBase<DeviceStateGanttChartFieldDesign>(design)
     {
@@ -14,7 +18,6 @@ namespace Codeer.LowCode.Bindings.ORiN3.Fields
         public override bool IsModified => false;
         public override FieldDataBase? GetData() => null;
         public override FieldSubmitData GetSubmitData() => new();
-        public override async Task InitializeDataAsync(FieldDataBase? fieldDataBase) => await Task.CompletedTask;
         public override async Task SetDataAsync(FieldDataBase? fieldDataBase) => await Task.CompletedTask;
 
         [ScriptMethodToProperty("Value")]
@@ -23,6 +26,53 @@ namespace Codeer.LowCode.Bindings.ORiN3.Fields
             if (Value == value) return;
             Value = value;
             NotifyStateChanged();
+        }
+
+        public override async Task InitializeDataAsync(FieldDataBase? fieldDataBase)
+        {
+            await Task.CompletedTask;
+            Machines = CreateData();
+        }
+
+        public List<MachineRow> Machines { get; private set; } = new();
+
+        /* ===== Sample data ===== */
+        List<MachineRow> CreateData()
+        {
+            var random = new Random();
+
+            var list = new List<MachineRow>();
+            foreach(var line in Design.DeviceAndMachinery)
+            {
+                var sp = line.Split(',').Select(e => e.Trim()).ToArray();
+                if (sp.Length != 2) continue;
+                var name = sp[1];
+                //var device = sp[0]; TODO
+
+                var segments = new List<Segment>();
+
+                //TODO sample Design.StartHour, Design.EndHour, Design.StepMin, Design.StateAndTextAndColor
+
+                for (var i = Design.StartHour; i < Design.EndHour; i++)
+                {
+                    for (var j = 0; j < 60; j += Design.StepMin)
+                    {
+                        var color = "#ffffff";
+                        if (Design.StateAndTextAndColor.Any())
+                        {
+                            var sp2 = Design.StateAndTextAndColor[random.Next(Design.StateAndTextAndColor.Count)].Split(',');
+                            if (sp2.Length == 3)
+                            {
+                                color = sp2[2];
+                            }
+                        }
+                        segments.Add(new Segment(color));
+                    }
+                }
+
+                list.Add(new MachineRow(name, segments));
+            }
+            return list;
         }
     }
 }
