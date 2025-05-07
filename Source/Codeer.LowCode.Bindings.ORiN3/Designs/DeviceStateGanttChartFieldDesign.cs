e649e5bfb7c4ea360171f18e1d49af0185cf3a15
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2025-05-07 16:34:21 +0900

    Add DeviceStateGanttChartField

---
+++ Source/Codeer.LowCode.Bindings.ORiN3/Designs/DeviceStateGanttChartFieldDesign.cs
@@ -0,0 +1,18 @@
+﻿using Codeer.LowCode.Bindings.ORiN3.Components;
+using Codeer.LowCode.Bindings.ORiN3.Fields;
+using Codeer.LowCode.Blazor.OperatingModel;
+using Codeer.LowCode.Blazor.Repository.Data;
+using Codeer.LowCode.Blazor.Repository.Design;
+
+namespace Codeer.LowCode.Bindings.ORiN3.Designs
+{
+    [ToolboxIcon(PackIconMaterialKind = "ProgressStar")]
+    public class DeviceStateGanttChartFieldDesign() : FieldDesignBase(typeof(DeviceStateGanttChartFieldDesign).FullName!)
+    {
+        public override string GetWebComponentTypeFullName() => typeof(DeviceStateGanttChartFieldComponent).FullName!;
+        public override string GetSearchWebComponentTypeFullName() => string.Empty;
+        public override string GetSearchControlTypeFullName() => string.Empty;
+        public override FieldBase CreateField() => new DeviceStateGanttChartField(this);
+        public override FieldDataBase? CreateData() => null;
+    }
+}
---
+++ Source/Codeer.LowCode.Bindings.ORiN3/Fields/DeviceStateGanttChartField.cs
@@ -0,0 +1,28 @@
+﻿using Codeer.LowCode.Bindings.ORiN3.Designs;
+using Codeer.LowCode.Blazor.DataIO;
+using Codeer.LowCode.Blazor.OperatingModel;
+using Codeer.LowCode.Blazor.Repository.Data;
+using Codeer.LowCode.Blazor.Script;
+
+namespace Codeer.LowCode.Bindings.ORiN3.Fields
+{
+    public class DeviceStateGanttChartField(DeviceStateGanttChartFieldDesign design)
+        : FieldBase<DeviceStateGanttChartFieldDesign>(design)
+    {
+        public double Value { get; private set; }
+
+        public override bool IsModified => false;
+        public override FieldDataBase? GetData() => null;
+        public override FieldSubmitData GetSubmitData() => new();
+        public override async Task InitializeDataAsync(FieldDataBase? fieldDataBase) => await Task.CompletedTask;
+        public override async Task SetDataAsync(FieldDataBase? fieldDataBase) => await Task.CompletedTask;
+
+        [ScriptMethodToProperty("Value")]
+        public void SetValue(double value)
+        {
+            if (Value == value) return;
+            Value = value;
+            NotifyStateChanged();
+        }
+    }
+}
